df4d575fe0f63983f1885e6060c53d1268421aec
alphamunene <114361783+alphamunene@users.noreply.github.com>
2025-07-10 13:51:15 +0300

    Update server.py

--- load_balancer_project (2)/server/server.py
+++ load_balancer_project (2)/server/server.py
@@ -6,11 +6,14 @@ app = Flask(__name__)
 @app.route('/home', methods=['GET'])
 def home():
     server_id = os.environ.get("SERVER_ID", "Unknown")
-    return jsonify({"message": f"Hello from Server: {server_id}", "status": "successful"}), 200
+    return jsonify({
+        "message": f"Hello from Server: {server_id}",
+        "status": "successful"
+    })
 
 @app.route('/heartbeat', methods=['GET'])
 def heartbeat():
-    return "", 200
+    return '', 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
